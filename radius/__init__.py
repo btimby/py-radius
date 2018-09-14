14301ab36caf57ccd5e3b20e72728ea68bf69296
Matt Wheeler <m@funkyhat.org>
2018-09-14 14:43:12 +0100

    add requirements to setup(install_requires=)

--- radius/__init__.py
+++ radius/__init__.py
@@ -58,8 +58,8 @@ except ImportError:
 
 from six import PY3
 
+from .__version__ import __version__  # noqa: F401
 
-__version__ = '2.0.2'
 
 LOGGER = logging.getLogger(__name__)
 LOGGER.addHandler(logging.NullHandler())
--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 #!/bin/env python
 
 from distutils.core import setup
-import radius
+
+exec(open('radius/__version__.py').read())  # not possible to import at install time
 
 name = 'py-radius'
-version = radius.__version__
 release = '1'
-versrel = version + '-' + release
+versrel = __version__ + '-' + release
 download_url = 'https://github.com/downloads/btimby/' + name + \
                            '/' + name + '-' + versrel + '.tar.gz'
 
 with open('LICENSE', 'r') as l:
     license = l.read()
+
+
+with open('requirements.txt') as f:
+    install_requires = [line.strip() for line in f]
+
 
 setup(
     name = name,
@@ -26,7 +31,7 @@ setup(
     url = 'http://github.com/btimby/' + name + '/',
     download_url = download_url,
     license = license,
-    py_modules = ["radius"],
+    packages = ["radius"],
     classifiers = [
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
@@ -39,5 +44,6 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: System :: Systems Administration :: Authentication/Directory',
-    ]
+    ],
+    install_requires=install_requires,
 )
